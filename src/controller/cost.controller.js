9021218a5c3d863478c6e8805bbd5d97d673b89b
Jewel Mia <145703213+jewel1269@users.noreply.github.com>
2025-04-18 02:07:18 +0600

    fix some error

--- src/controller/cost.controller.js
+++ src/controller/cost.controller.js
@@ -1,4 +1,5 @@
 import Cost from "../models/cost.model.js";
+import { ObjectId } from "mongodb";
 
 
 export const AddCost = async (req, res, next) => {
@@ -17,7 +18,7 @@ export const AddCost = async (req, res, next) => {
       return res.status(400).json({ message: "সব প্রয়োজনীয় তথ্য দিন।" });
     }
 
-    const newIncome = new Cost({
+    const newCost = new Cost({
       email,
       cost,
       category,
@@ -27,13 +28,13 @@ export const AddCost = async (req, res, next) => {
       attachmentImage,
     });
 
-    await newIncome.save();
+    await newCost.save();
 
-    console.log(newIncome);
+    console.log(newCost);
 
     res.status(200).json({
       message: "খরচ  সফলভাবে যোগ হয়েছে ✅",
-      data: newIncome,
+      data: newCost,
     });
   } catch (err) {
     console.error("খরচ  যোগ করতে সমস্যা:", err);
@@ -42,22 +43,55 @@ export const AddCost = async (req, res, next) => {
 };
 
 export const getAllCost = async (req, res, next) => {
-    try {
-      const { email } = req.params;
-  
-      if (!email) {
-        return res.status(400).json({ message: "ইমেইল প্রদান করা হয়নি।" });
-      }
-  
-      const allCost = await Cost.find({ email }).sort({ date: -1 }); 
-  
-      res.status(200).json({
-        success:true,
-        message:"Data Successfully retrive",
-        data: allCost
-      });
-    } catch (err) {
-      next(err)
-      res.status(500).json({ message: "সার্ভার ত্রুটি।" });
+  try {
+    const { email } = req.params;
+
+    if (!email) {
+      return res.status(400).json({ message: "ইমেইল প্রদান করা হয়নি।" });
     }
-  };
+
+    const allCost = await Cost.find({ email }).sort({ date: -1 });
+
+    res.status(200).json({
+      success: true,
+      message: "Data Successfully retrive",
+      data: allCost,
+    });
+  } catch (err) {
+    next(err);
+    res.status(500).json({ message: "সার্ভার ত্রুটি।" });
+  }
+};
+
+export const deleteCost = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res
+        .status(400)
+        .json({ success: false, message: "আইডি প্রয়োজন!" });
+    }
+
+    const deleted = await Cost.findOneAndDelete({ _id: new ObjectId(id) });
+
+    if (!deleted) {
+      return res.status(404).json({
+        success: false,
+        message: "ডেটা পাওয়া যায়নি বা ইতিমধ্যে ডিলিট হয়েছে!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "ডেটা সফলভাবে ডিলিট হয়েছে!",
+    });
+  } catch (err) {
+    next(err);
+    console.error("deleteSemester Error:", err.message);
+    res.status(500).json({
+      success: false,
+      message: "সার্ভার ত্রুটি!",
+    });
+  }
+};
--- src/routes/cost.route.js
+++ src/routes/cost.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { AddCost, getAllCost } from "../controller/cost.controller.js";
+import { AddCost, deleteCost, getAllCost } from "../controller/cost.controller.js";
 const router = express.Router();
 
 router.post("/add", AddCost);
 router.get("/:email", getAllCost);
+router.delete("/delete/:id", deleteCost);
 
 
 export default router;
